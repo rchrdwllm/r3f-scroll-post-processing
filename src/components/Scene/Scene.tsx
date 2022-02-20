829d55389b3215dd7c6a0142ffac9e1be0fce02a
Richard William <rwilliam.flores@gmail.com>
2022-02-20 14:55:34 +0800

    added zoom effect

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,6 +1,6 @@
 import data from "data";
 import { useThree } from "@react-three/fiber";
-import { Image } from "@react-three/drei";
+import Image from "components/Image";
 
 const Scene = () => {
     const {
@@ -12,22 +12,27 @@ const Scene = () => {
             <Image
                 url={data[0]}
                 position={[width / 4, 0, 1]}
-                scale={[width / 2, height, 0] as any}
+                scale={[width / 2, height, 20, 20] as any}
+                zoom={[0, 1, 0.5]}
             />
             <Image
                 url={data[1]}
                 position={[-3, -height, 0]}
-                scale={[4, 5, 0] as any}
+                scale={[4, 4, 20, 20] as any}
+                zoom={[0.2, 1, 0.5]}
             />
             <Image
                 url={data[2]}
                 position={[2, -height - 1, 2]}
-                scale={[3, 4, 0] as any}
+                scale={[3, 4, 20, 20] as any}
+                zoom={[1.5 / 3, 1, 0.5]}
             />
             <Image
                 url={data[3]}
                 position={[0, -height * 2 - height / 4, 1]}
-                scale={[width, height / 1.25, 0] as any}
+                scale={[width, height / 1.5, 20, 20] as any}
+                zoom={[2 / 3, 2]}
+                zoomReverse
             />
         </group>
     );
