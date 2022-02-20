d9136cd001b51af0b13861832c879b248e31a869
Richard William <rwilliam.flores@gmail.com>
2022-02-20 14:54:37 +0800

    optimized for mobile use

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,22 +1,25 @@
 import type { FunctionComponent } from "react";
 import styles from "./Canvas.module.scss";
+import useMobileDetect from "use-mobile-detect-hook";
 import { Canvas as FiberCanvas } from "@react-three/fiber";
 import { ScrollControls, Scroll } from "@react-three/drei";
 import Scene from "components/Scene";
 import Effects from "components/Effects";
 
 const Canvas: FunctionComponent = ({ children }) => {
+    const { isMobile }: { isMobile: () => true } = useMobileDetect();
+
     return (
         <div className={styles.canvasContainer}>
             <FiberCanvas
                 camera={{ position: [0, 0, 5] }}
                 onCreated={({ gl }) => {
-                    gl.setClearColor(0x1a1a1a, 1);
+                    gl.setClearColor(0x141414, 1);
                 }}
             >
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <ScrollControls pages={3} damping={6}>
+                <ScrollControls pages={3} damping={isMobile() ? 20 : 6}>
                     <Scroll>
                         <Scene />
                     </Scroll>
